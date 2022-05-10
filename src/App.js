4737aeb65bfe6cd5efd5f4e95acd94449e75dddd
jlemke <jlemke3@madisoncollege.edu>
2022-05-10 11:26:56 -0500

    Modified app to work with updated api
    Added input fields and state to src/App.js

--- src/App.js
+++ src/App.js
@@ -7,15 +7,24 @@ import { API } from 'aws-amplify';
 import './App.css';
 
 const App = () => {
+
+  // Create additional state to hold user input for limit and start properties
+  const [input, updateInput] = useState({ limit: 5, start: 0 })
+
+  // Create a new function to allow users to update the input values
+  const updateInputValues = (type, value) => {
+    updateInput({ ...input, [type]: value });
+  };
+
   // Create coins variable and set to empty array
   const [coins, updateCoins] = useState([]);
 
-  // Define function to all API
+  // Update fetchCoins function to use limit and start properties
   const fetchCoins = async () => {
-    const data = await API.get('cryptoapi', '/coins');
-    console.log(data);
+    const { limit, start } = input;
+    const data = await API.get('cryptoapi', `/coins?limit=${limit}&start=${start}`);
     updateCoins(data.coins);
-  };
+  }
 
   // Call fetchCoins function when component loads
   useEffect(() => {
@@ -24,6 +33,20 @@ const App = () => {
 
   return (
     <div className="App">
+      <input
+        placeholder="limit"
+        onChange={e => updateInputValues('limit', e.target.value)}
+      />
+      <input
+        placeholder="start"
+        onChange={e => updateInputValues('start', e.target.value)}
+      />
+
+      <button 
+        onClick={fetchCoins}
+      >
+        Fetch Coins
+      </button>
       {
         coins.map((coin) => (
           <div key={coin.name}>
